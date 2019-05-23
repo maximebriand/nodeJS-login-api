eea3d0bcc59afff5e9a64f8ab71529c68a8128be
Maxime Briand <contact@maximebriand.fr>
2019-05-23 21:53:13 +0200

    add login route

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const User = require('../models/users.model')
+const bcrypt = require('bcrypt')
 
 
 exports.addUser = (req, res) => {
@@ -12,8 +13,6 @@ exports.addUser = (req, res) => {
     const user = new User({ name: name, password: password, email: email, role: 'user' }); // document = instance of a model
     // TODO: We can hash the password here before we insert instead of in the model
     user.save((err, user) => {
-        console.log('err')
-        console.log(err)
         if (!err) {
             result.status = status;
             result.result = user;
@@ -25,4 +24,38 @@ exports.addUser = (req, res) => {
         res.status(status).send(result);
     });
    
+}
+
+exports.userLogin = (req, res) => {
+    const { name, password } = req.body;
+    let result = {};
+    let status = 200;
+
+    User.findOne({name}, (err, user) => {
+        if (!err && user) {
+          // We could compare passwords in our model instead of below
+          bcrypt.compare(password, user.password).then(match => {
+            if (match) {
+              result.status = status;
+              result.result = user;
+            } else {
+              status = 401;
+              result.status = status;
+              result.error = 'Authentication error';
+            }
+            res.status(status).send(result);
+          }).catch(err => {
+            status = 500;
+            result.status = status;
+            result.error = err;
+            res.status(status).send(result);
+          });
+        } else {
+          status = 404;
+          result.status = status;
+          result.error = err;
+          res.status(status).send(result);
+        }
+    })
+
 }
--- app/routes/users.route.js
+++ app/routes/users.route.js
@@ -2,5 +2,8 @@ const controller = require('../controllers/users.controller');
 
 module.exports = (router) => {
   router.route('/users')
-    .post(controller.addUser);
+    .post(controller.addUser)
+
+    router.route('/login')
+    .post(controller.userLogin)
 }
